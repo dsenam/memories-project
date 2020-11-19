417f3414d6fd6fc7c36e1ccb35ca4ee88de45b0e
INDRA\dsenam <dsenam@indracompany.com>
2020-11-19 15:04:18 -0300

    + conexão com mongoDB

--- backend/index.js
+++ backend/index.js
@@ -10,5 +10,16 @@ app.use(bodyParser.json({limit:"30mb", extended: true }));
 app.use(bodyParser.urlencoded({limit:"30mb", extended: true }));
 app.use(cors());
 
+//criando conexão com o MONGODB
+const CONNECTION_URL = 'mongodb+srv://admin:admin@cluster0.wtfcj.mongodb.net/<dbname>?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+.then(() => app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`)))
+.catch((error)=> console.log(error.message))
+
+mongoose.set('useFindAndModify', false)
 
 
